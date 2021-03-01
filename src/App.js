681461e744c7c9f2b6ef9ae67f3a739465a3cef2
Alina Zinchenko <c0zial00@students.oamk.fi>
2021-03-01 19:09:15 +0200

    tiedon lisäys ok

--- src/App.js
+++ src/App.js
@@ -1,23 +1,86 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState, useEffect} from 'react';
+
+const URL = 'http://localhost/shoppinglist/';
+
 
 function App() {
+
+  const [items, setItems] = useState([]);
+  const [item, setItem] = useState('');
+  const [amount, setAmount] = useState('');
+
+  useEffect(() => {
+    let status = 0;
+    fetch(URL + 'index.php')
+    .then(res => {
+      status = parseInt(res.status);
+      return res.json()
+    })
+    .then(
+      (res) => {
+        if (status === 200) {
+          setItems(res);
+        } else {
+          alert(res.error);
+        }
+      }, (error) => {
+        alert(error);
+      }
+    )
+  }, [])
+
+  function add(e) {
+    e.preventDefault();
+    let status = 0;
+    fetch(URL + 'add.php', {
+      method: 'POST',
+      headers: {
+        'Accept': 'application/json',
+        'Content-type': 'application/json',
+      },
+      body: JSON.stringify({
+        description: item,
+        amount: amount
+      })
+    })
+    .then(res => {
+      status = parseInt(res.status);
+      return res.json()
+    })
+    .then(
+      (res) => {
+        if (status === 200) {
+          setItems(items => [...items,res]);
+          //kenttä tyhjenee
+          setItem('');
+          setAmount('');
+        } else {
+          alert(res.error);
+        }
+      }, (error) => {
+        alert(error);
+      }
+    )
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='container'>
+      <h2>Shopping list</h2>
+      <form onSubmit={add}>
+        <div>
+          <label>Item: </label>
+          <input type="text" value={item} onChange={e => setItem(e.target.value)} />
+          <label>Amount: </label>
+          <input type="number" value={amount} onChange={e => setAmount(e.target.value)}/>
+          <button>Add</button>
+        </div>
+      </form>
+      <ol>
+        {items.map(item => (
+          <li key={item.id}>{item.description} - {item.amount} pc.<a onClick={() => delete(item.id)} href="#">Delete</a></li>
+        ))}
+      </ol>
     </div>
   );
 }
